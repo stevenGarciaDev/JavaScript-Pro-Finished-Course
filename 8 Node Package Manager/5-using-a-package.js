55bb34e0f7c47d394c2fcb44b81af5acd78bc4fb
Steven Garcia <stevgarcia2015@gmail.com>
2024-12-12 11:45:32 -0800

    Add notes for section 8 'Node Package Manager'

---
+++ 8 Node Package Manager/5-using-a-package.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => console.log(response.data))
+    .catch(err => console.error('Error: ', err.message));
